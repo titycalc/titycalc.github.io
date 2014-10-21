bc8d018a60188086008dfcd21e1c2f4e36965b17
tity <titycalc@gmail.com>
2014-10-21 20:01:32 +0900

    Delete commentouts

--- sandbox/gcc/bison/main.c
+++ sandbox/gcc/bison/main.c
@@ -5,9 +5,5 @@
 int main(int argc, char **argv) {
   yyin = stdin;
   yyout = stdout;
-  /*yylloc.first_line = 1;
-  yylloc.last_line = 1;
-  yylloc.first_column = 0;
-  yylloc.last_column = 0;*/
   return yyparse();
 }
